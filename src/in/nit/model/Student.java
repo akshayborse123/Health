091171ab5a58ee01508d7f1464cdc8ba190afc76
Akshay <Akshay@DESKTOP-B4HDOG4>
2020-02-08 20:28:57 +0530

    ns-2443

---
+++ src/in/nit/model/Student.java
@@ -0,0 +1,14 @@
+package in.nit.model;
+
+public class Student {
+	private String sname;
+
+	public String getSname() {
+		return sname;
+	}
+
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+
+}
